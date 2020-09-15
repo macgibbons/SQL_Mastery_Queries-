a73cc4171fe9984f8e848e706409a9768458ff96
Mac Gibbons <macgibbons@gmail.com>
2020-09-14 23:23:28 -0500

    added chapter 7 and

---
+++ book3/Chapter 7/Chapter_7.sql
@@ -0,0 +1,9 @@
+-- 1. Write a transaction to:
+--   - Add a new role for employees called `Automotive Mechanic`
+--   - Add five new mechanics, their data is up to you
+--   - Each new mechanic will be working at all three of these dealerships: Sollowaye Autos of New York, Hrishchenko Autos of New York and Cadreman Autos of New York
+-- 2. Create a transaction for:
+--   - Creating a new dealership in Washington, D.C. called Felphun Automotive
+--   - Hire 3 new employees for the new dealership: Sales Manager, General Manager and Customer Service.
+--   - All employees that currently work at Scrogges Autos of District of Columbia will now start working 
+--     at Felphun Automotive instead.
